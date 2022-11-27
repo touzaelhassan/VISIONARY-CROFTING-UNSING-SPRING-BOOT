6f91eceee4599a7ab9bf288155fc678468ed0062
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-11-27 09:53:49 +0100

    add crud de stock

---
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/StockService.java
@@ -0,0 +1,33 @@
+package com.visionarycrofting.service;
+
+import com.visionarycrofting.entity.Stock;
+import com.visionarycrofting.repository.IStockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockService {
+
+    @Autowired
+    IStockRepository stockRepository;
+
+    public Stock saveStock(Stock stock){
+        stockRepository.save(stock);
+        return stock;
+    }
+
+    public List<Stock> getAllStock(){
+        return stockRepository.findAll();
+    }
+
+    public Stock updateStock(Stock stock){
+        stockRepository.save(stock);
+        return stock;
+    }
+
+    public Stock findOneStock(int idStock){
+        return stockRepository.findById(idStock).get();
+    }
+}
