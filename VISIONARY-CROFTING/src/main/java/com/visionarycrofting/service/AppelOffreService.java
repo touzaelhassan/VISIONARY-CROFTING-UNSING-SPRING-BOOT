6137adc448266a717d84954cdb3d815cebad60d0
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-28 09:22:53 +0100

    create methode to get all

--- VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/AppelOffreService.java
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/AppelOffreService.java
@@ -1,8 +1,12 @@
 package com.visionarycrofting.service;
 
+import com.visionarycrofting.entity.AppelOffre;
 import com.visionarycrofting.repository.IAppelOffreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class AppelOffreService {
@@ -12,6 +16,8 @@ public class AppelOffreService {
     public AppelOffreService(IAppelOffreRepository appelOffreRepository) {
         this.appelOffreRepository = appelOffreRepository;
     }
-
+    public List<AppelOffre> getAppelOffres(){
+        return appelOffreRepository.findAll();
+    }
 
 }
