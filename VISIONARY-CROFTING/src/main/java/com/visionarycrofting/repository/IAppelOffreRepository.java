c0a0691d2042dde30189146c9aba0a5966948893
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-28 09:12:36 +0100

    Create Interface Repository of AppelOffre and extend JpaRepository

---
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/repository/IAppelOffreRepository.java
@@ -0,0 +1,7 @@
+package com.visionarycrofting.repository;
+
+import com.visionarycrofting.entity.AppelOffre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAppelOffreRepository extends JpaRepository<AppelOffre,Integer> {
+}
