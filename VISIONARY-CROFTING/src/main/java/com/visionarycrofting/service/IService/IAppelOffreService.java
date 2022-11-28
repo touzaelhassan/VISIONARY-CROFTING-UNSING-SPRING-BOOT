68f2382405c83fdc5772e82f815eedbe33e5ceba
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-28 11:20:39 +0100

    Create delete methode

--- VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/IService/IAppelOffreService.java
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/IService/IAppelOffreService.java
@@ -1,6 +1,8 @@
 package com.visionarycrofting.service.IService;
 
 import com.visionarycrofting.entity.AppelOffre;
+import com.visionarycrofting.entity.Produit;
+import com.visionarycrofting.entity.StatusAppelOffre;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface IAppelOffreService {
     public void addAppelOffre(AppelOffre appelOffre);
 
     public void deleteAppelOffre(Integer id);
+
+    public void updateAppelOffre(Integer id, String reference, StatusAppelOffre statusAppelOffre, Produit produit);
 }
