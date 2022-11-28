cb07948c10079f6a36ca3da4c97dee10e7ec6ecb
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-28 09:19:37 +0100

    Add repository annotation

--- VISIONARY-CROFTING/src/main/java/com/visionarycrofting/repository/IAppelOffreRepository.java
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/repository/IAppelOffreRepository.java
@@ -2,6 +2,8 @@ package com.visionarycrofting.repository;
 
 import com.visionarycrofting.entity.AppelOffre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAppelOffreRepository extends JpaRepository<AppelOffre,Integer> {
 }
