207df10cf6e1b5e9713eeb6cc8a590d98e1b27f5
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-29 16:34:45 +0100

    Add return to add method

--- VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/IService/IAppelOffreService.java
+++ VISIONARY-CROFTING/src/main/java/com/visionarycrofting/service/IService/IAppelOffreService.java
@@ -4,12 +4,11 @@ import com.visionarycrofting.entity.AppelOffre;
 import com.visionarycrofting.entity.StatusAppelOffre;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IAppelOffreService {
     public List<AppelOffre> getAppelOffres();
 
-    public void addAppelOffre(AppelOffre appelOffre);
+    public AppelOffre addAppelOffre(AppelOffre appelOffre);
 
     public void deleteAppelOffre(Integer id);
 
