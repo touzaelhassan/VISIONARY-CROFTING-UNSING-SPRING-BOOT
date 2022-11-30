7e063128a502723c5425e1b8eacab0100ed00c55
touzaelhassan <touzaelhassan@gmail.com>
2022-11-30 20:33:17 +0100

    Creating create and update method

--- VISIONARY-CROFTING/src/test/java/com/visionarycrofting/Service/ProduitServiceTest.java
+++ VISIONARY-CROFTING/src/test/java/com/visionarycrofting/Service/ProduitServiceTest.java
@@ -2,13 +2,13 @@ package com.visionarycrofting.Service;
 
 import com.visionarycrofting.entity.Category;
 import com.visionarycrofting.entity.Produit;
+import com.visionarycrofting.entity.Stock;
 import com.visionarycrofting.service.IService.IProduitService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProduitServiceTest {
@@ -18,7 +18,6 @@ class ProduitServiceTest {
 
     @Test
     void addProduit() {
-
 
         int produitNumbers = iProduitService.getAllProduits().size();
 
@@ -42,12 +41,39 @@ class ProduitServiceTest {
 
         int produitNumbersAfter = iProduitService.getAllProduits().size();
 
-
         assertThat(produitNumbers).isNotEqualTo(produitNumbersAfter);
-
 
     }
 
+    @Test
+    void updateProduit() {
 
+        Produit previousProduit = null;
+        String nom = "Produit test";
+        String reference = "444";
+        String description = "Produit description";
+        Float prix_initial = 15F;
+        int quantity = 10;
+        Produit produit = iProduitService.getProduitById(1L);
+        assertThat(produit).isNotNull();
+        previousProduit = iProduitService.getProduitById(1L);
+
+        assertThat(nom).isNotEmpty().isNotNull();
+        assertThat(quantity).isNotNull();
+        assertThat(prix_initial).isNotNull();
+        assertThat(description).isNotEmpty().isNotNull();
+        assertThat(reference).isNotEmpty().isNotNull();
+
+        produit.setNom(nom);
+        produit.setReference(reference);
+        produit.setDescription(description);
+        produit.setPrix_initial(prix_initial);
+        produit.setQuantity(quantity);
+
+        Produit produitNext =  iProduitService.updateProduit(produit, 1L);
+
+        assertThat(produitNext).isNotEqualTo(previousProduit);
+
+    }
 
 }
